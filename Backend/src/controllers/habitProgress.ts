24f9865a43509157c5b5aad5d4c286d2b0541b00
Hassan Sajid <khokharhassan81@yahoo.com>
2025-09-10 12:01:48 +0500

    Habit-Stalker

--- Backend/src/controllers/habitProgress.ts
+++ Backend/src/controllers/habitProgress.ts
@@ -184,6 +184,6 @@ export const checkout = async (req: Request, res: Response) => {
     res.status(200).json({ message: "Checkout Successfull" });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
-    console.log(err);
+    console.log(err); 
   }
 };
